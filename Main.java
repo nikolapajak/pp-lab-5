2d4ecfd4d4c7693630f53909db77da9e4a72534e
Nikola <nikolapajak2706@gmail.com>
2024-05-04 18:19:10 +0200

    zad1

---
+++ Main.java
@@ -0,0 +1,15 @@
+import company.beans.Person;
+import company.exceptions.InvalidAgeException;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try {
+            Person person = new Person("Harry", 30);
+            System.out.println("Name: " + person.getName());
+            System.out.println("Age: " + person.getAge());
+        } catch (InvalidAgeException e) {
+            System.out.println("Invalid age provided: " + e.getMessage());
+        }
+    }
+}

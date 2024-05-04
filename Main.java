7406ffa5124fb71cb2bbf653b27c87285488ff79
Nikola <nikolapajak2706@gmail.com>
2024-05-04 18:45:47 +0200

    zad3

--- Main.java
+++ Main.java
@@ -1,8 +1,12 @@
 import company.implementations.EmailMessenger;
+import company.utils.MathUtils;
 
 public class Main {
     public static void main(String[] args) {
+        int result = MathUtils.add(10, 5);
+        System.out.println("Result of addition: " + result);
+
         EmailMessenger emailMessenger = new EmailMessenger();
-        emailMessenger.sendMessage("Hello, this is an email message!");
+        emailMessenger.sendMessage("The result of addition is: " + result);
     }
 }

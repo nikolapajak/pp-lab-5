db67e478f83a4f30bb1409123f0f7903a00ec6e1
Nikola <nikolapajak2706@gmail.com>
2024-05-04 18:32:49 +0200

    zad2

--- Main.java
+++ Main.java
@@ -1,15 +1,8 @@
-import company.beans.Person;
-import company.exceptions.InvalidAgeException;
+import company.implementations.EmailMessenger;
 
 public class Main {
-
     public static void main(String[] args) {
-        try {
-            Person person = new Person("Harry", 30);
-            System.out.println("Name: " + person.getName());
-            System.out.println("Age: " + person.getAge());
-        } catch (InvalidAgeException e) {
-            System.out.println("Invalid age provided: " + e.getMessage());
-        }
+        EmailMessenger emailMessenger = new EmailMessenger();
+        emailMessenger.sendMessage("Hello, this is an email message!");
     }
 }

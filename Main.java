1fe725b7aa727ae69dd1eae6f12ec9c56b3a2a9b
Nikola <nikolapajak2706@gmail.com>
2024-05-04 18:52:46 +0200

    zad4

--- Main.java
+++ Main.java
@@ -1,12 +1,34 @@
+import company.beans.Person;
+import company.exceptions.InvalidAgeException;
 import company.implementations.EmailMessenger;
+import company.interfaces.Messenger;
 import company.utils.MathUtils;
 
 public class Main {
     public static void main(String[] args) {
-        int result = MathUtils.add(10, 5);
-        System.out.println("Result of addition: " + result);
 
-        EmailMessenger emailMessenger = new EmailMessenger();
-        emailMessenger.sendMessage("The result of addition is: " + result);
+        Person[] people = new Person[5];
+        
+        int b = 10;
+
+        Messenger messenger = new EmailMessenger();
+
+        try {
+            people[0] = new Person("John Doe", 30);
+            people[1] = new Person("Harry Potter", 25);
+            people[2] = new Person("Alice Wizard", 40);
+            people[3] = new Person("Johny Bravo", 35);
+            people[4] = new Person("Jan Bach", 50);
+
+
+            for (Person person : people) {
+                int age = person.getAge();
+                int sum = MathUtils.add(age, b);
+
+                messenger.sendMessage("Suma wieku " + person.getName() + " i wartości b: " + sum);
+            }
+        } catch (InvalidAgeException e) {
+            System.out.println("Invalid age detected: " + e.getMessage());
+        }
     }
 }
